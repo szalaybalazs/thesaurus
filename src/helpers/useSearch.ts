2a7759022f1a4056184370f393439754bee7500e
Szalay Balázs <szalay.balazs.ms@gmail.com>
2022-08-31 22:04:56 +0200

    added instant text fill from selection

--- src/helpers/useSearch.ts
+++ src/helpers/useSearch.ts
@@ -1,7 +1,17 @@
-import { useState } from "react";
+import { getSelectedText } from "@raycast/api";
+import { useState, useEffect } from "react";
 
 export const useSearch = (initial?: string) => {
   const [query, setQuery] = useState(initial || "");
+
+  useEffect(() => {
+    getSelectedText().then((text) => {
+      if (text.split(" ").length > 1) return;
+      if (text.length > 100) return;
+
+      setQuery(text.trim());
+    });
+  }, []);
 
   return {
     query,
